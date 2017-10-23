9f6e7f69b9bd175de1f9eb403b50537ddc603968
Alexander Solovov <a_solovov@yahoo.com>
2017-10-23 14:45:52 +0300

    New filed Area Gross added

---
+++ changeset/1710a.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO system.version SELECT '1710a' WHERE NOT EXISTS (SELECT version_num FROM system.version WHERE version_num = '1710a');
+
+ALTER TABLE opentenure.claim ADD COLUMN area_gross double precision;
+COMMENT ON COLUMN opentenure.claim.area_gross IS 'Area Gross size';
+
+ALTER TABLE opentenure.claim_historic ADD COLUMN area_gross double precision;
